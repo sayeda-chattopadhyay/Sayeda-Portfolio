158ba3d895d56c400826cc1d110f38343de28fb7
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2022-10-16 22:14:48 +0200

    fixed header and main issue

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,16 +1,39 @@
-const sideNavMenu = document.querySelector("#side-nav-menu");
-console.log(sideNavMenu);
+// const sideNavMenu = document.querySelector("#side-nav-menu");
+// console.log(sideNavMenu);
 
-const close = document.querySelector(".close");
-console.log(close);
+// const close = document.querySelector(".close");
+// console.log(close);
+
+// const menu = document.querySelector(".menu");
+// console.log(menu);
+
+// menu.addEventListener("click", () => {
+//   sideNavMenu.style.right = "0";
+// });
+
+// close.addEventListener("click", () => {
+//   sideNavMenu.style.right = "-200px";
+// });
+
+// new code
 
 const menu = document.querySelector(".menu");
-console.log(menu);
+const close = document.querySelector(".close");
+const nav = document.querySelector("#main-nav");
 
 menu.addEventListener("click", () => {
-  sideNavMenu.style.right = "0";
+  nav.classList.add("open-nav");
 });
 
 close.addEventListener("click", () => {
-  sideNavMenu.style.right = "-200px";
+  nav.classList.remove("open-nav");
+});
+
+//teacher´s code
+
+document.body.addEventListener("click", function (event) {
+  console.dir(event.target);
+  if (event.target.id !== "side-nav-menu" && event.target.id !== "open-menu") {
+    sideNavMenu.style.right = "-200px";
+  }
 });
